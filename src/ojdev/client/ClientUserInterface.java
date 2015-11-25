ab3ed223dec614efb368224672536901efe0e135
Joshua Neuenhoff <wiirh1@live.com>
2015-11-24 21:24:11 -0500

    Add notification of Warrior change to ClientUserInterface

--- src/ojdev/client/ClientUserInterface.java
+++ src/ojdev/client/ClientUserInterface.java
@@ -1,8 +1,12 @@
 package ojdev.client;
 
+import ojdev.common.warriors.WarriorBase;
+
 /**
  * Interface used by Client to communicate with the UI
  */
 public interface ClientUserInterface {
 	public abstract void notifyException(Exception e);
+
+	public abstract void notifyCurrentWarriorChanged(WarriorBase currentWarrior);
 }
